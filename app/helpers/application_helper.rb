af31f0249837ef08528d9038b3a4645cc621adf6
Edward Guo <edwardplguo@gmail.com>
2015-08-06 11:18:54 -0400

    Change post and posts' layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,9 @@ module ApplicationHelper
   def white_spaces(n=1)
     raw( "&nbsp;" * n )
   end
+
+  def markdown(text)
+    @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(escape_html: true, hard_wrap: true ), autolink: true, strikethrough: true, tables: true, underline: true)
+    raw(@markdown.render(text))
+  end
 end
