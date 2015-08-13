b1c8bcf66323366378c444dd45414e76f23e7027
Shalin1998 <shalin617@gmail.com>
2015-08-13 13:53:59 -0400

    some changes

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -77,6 +77,6 @@ class AttendancesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def attendance_params
-      a_params = params.require(:attendance).permit(:start_at, :end_at, :status, :skip_end_at)
+      a_params = params.require(:attendance).permit(:start_at, :end_at, :status, :duration, :skip_end_at)
     end
 end
