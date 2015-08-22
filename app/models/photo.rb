f001d3a1e5f115e4a18836c83a5d7f5e5374dd3d
Edward Guo <edwardplguo@gmail.com>
2015-08-22 06:55:48 -0400

    Add enum and validations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,11 @@ class Photo < ActiveRecord::Base
   mount_uploader :file, PhotoUploader
 
   has_many :photo_locations, inverse_of: [:photo, :processed_photo]
+
+  validate :at_least_one_source
+
+  private
+  def at_least_one_source
+    errors.add(:base, 'At least one source must be provided') unless self.file.present? || self.external_link.present?
+  end
 end
