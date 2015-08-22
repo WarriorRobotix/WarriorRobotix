0065899fccf01297d0e634cccc5c6377bccde1d9
Edward Guo <edwardplguo@gmail.com>
2015-08-21 21:30:25 -0400

    Fix photo location migration

--- db/migrate/20150822004804_create_photo_locations.rb
+++ db/migrate/20150822004804_create_photo_locations.rb
@@ -3,10 +3,13 @@ class CreatePhotoLocations < ActiveRecord::Migration
     create_table :photo_locations do |t|
       t.string :page, null: false, index: true
       t.string :location, null: false
+
       t.belongs_to :photo, index: true, foreign_key: true
       t.belongs_to :processed_photo, index: true, foreign_key: true
-      t.integer :width, null: false
-      t.integer :height, null: false
+
+      t.integer :dimension_type, default: 0, null: false
+      t.integer :width
+      t.integer :height
 
       t.timestamps null: false
     end
