f6fe1e6f416c7c32ffebc6acae2c5f8c07c76cda
Edward Guo <edwardplguo@gmail.com>
2015-08-21 21:31:39 -0400

    Add or fix Photos and PhotoLocations' relations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,5 @@
 class Photo < ActiveRecord::Base
   mount_uploader :file, PhotoUploader
+
+  has_many :photo_locations, inverse_of: [:photo, :processed_photo]
 end
--- app/models/photo_location.rb
+++ app/models/photo_location.rb
@@ -1,4 +1,4 @@
 class PhotoLocation < ActiveRecord::Base
-  belongs_to :photo
-  belongs_to :processed_photo
+  belongs_to :photo, inverse_of: :photo_locations
+  belongs_to :processed_photo, class_name: 'Photo', inverse_of: :photo_locations
 end
