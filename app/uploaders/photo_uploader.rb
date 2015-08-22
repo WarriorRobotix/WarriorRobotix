f6c5143e7ef67ecd088f85c7b33f4a6764bb2e5c
Edward Guo <edwardplguo@gmail.com>
2015-08-21 20:44:22 -0400

    Add PhotoUploader

---
+++ app/uploaders/photo_uploader.rb
@@ -0,0 +1,23 @@
+# encoding: utf-8
+
+class PhotoUploader < CarrierWave::Uploader::Base
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+
+  def filename
+    "#{original_filename}-#{secure_token(10)}.#{file.extension}" if original_filename.present?
+  end
+
+  protected
+  def secure_token(length=16)
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.hex(length/2))
+  end
+end
