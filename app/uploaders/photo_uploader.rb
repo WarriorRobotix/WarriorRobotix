39d5ef988d7aba8e7d7d6748d0a7ba09e66419b5
Edward Guo <edwardplguo@gmail.com>
2015-08-22 07:03:41 -0400

    Shorter photo store_dir

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -4,7 +4,7 @@ class PhotoUploader < CarrierWave::Uploader::Base
   storage :file
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "photos/#{model.id}"
   end
 
   def extension_white_list
