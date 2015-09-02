2c9c429646e95e1f714d22271c811bcf0f1fab67
Edward Guo <edwardplguo@gmail.com>
2015-09-02 10:21:29 -0400

    Fix email logo for gmail

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,11 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "no-reply@warrior-robotix.herokuapp.com"
+  before_action :attach_logo
+
+  default :from => "no-reply@warrior-robotix.herokuapp.com"
   layout 'mailer'
+
+  private
+  def attach_logo
+    attachments.inline['logo.png'] = File.read(Rails.root.join('app', 'assets', 'images', 'mailers', 'logo.png'))
+  end
 end
