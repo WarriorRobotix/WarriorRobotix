099f7f608eb72404a8c0c82750ee8a6a99f60ac3
Edward Guo <edwardplguo@gmail.com>
2015-09-02 09:31:55 -0400

    Use send grid for smtp email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,14 +70,15 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: ENV['MAILER_DEFAULT_HOST'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            'team4659.website@gmail.com',
-    password:             ENV['GMAIL_PASSWORD'],
-    authentication:       'plain',
-    enable_starttls_auto: true  }
-
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+  
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
