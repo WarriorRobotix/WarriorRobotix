84ce17a5656b6f0b0ab7e8a3f77e09adf0c66a12
Edward Guo <edwardplguo@gmail.com>
2015-09-05 13:40:18 -0400

    Allow quite rejections

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -218,24 +218,27 @@ class MembersController < ApplicationController
   def reject
     reason = params[:reason].blank? ? nil :  params[:reason]
     GlobalVar[:last_reject_members_reason] = reason
+    @quite_reject = params[:commit] == "Quite Reject"
     if params[:member] == 'all'
       pending_members = Member.unscoped.where(accepted: false)
-      pending_members.pluck(:first_name, :last_name, :email).map { |e| ["#{e[0]} #{e[1]}", e[2]] }.each do |pm|
-        MemberMailer.registration_rejected_email(pm[0], pm[1], reason).deliver_later
+      unless @quite_reject
+        pending_members.pluck(:first_name, :last_name, :email).map { |e| ["#{e[0]} #{e[1]}", e[2]] }.each do |pm|
+          MemberMailer.registration_rejected_email(pm[0], pm[1], reason).deliver_later
+        end
       end
       pending_members.destroy_all
     else
       if pending_member = Member.unscoped.where(id: params[:member].to_i, accepted: false).take
         pending_member.destroy
-        MemberMailer.registration_rejected_email(pending_member.full_name, pending_member.email, reason).deliver_later
+        MemberMailer.registration_rejected_email(pending_member.full_name, pending_member.email, reason).deliver_later unless @quite_reject
       end
     end
 
     respond_to do |format|
       if params[:member] == 'all'
-        format.html { redirect_to members_url, notice: "All pending members were successfully rejected." }
+        format.html { redirect_to members_url, notice: "All pending members were successfully#{" and quietly" if @quite_reject} rejected." }
       else
-        format.html { redirect_to members_url, notice: "#{pending_member.try(:full_name) || 'A pending member'} was successfully rejected." }
+        format.html { redirect_to members_url, notice: "#{pending_member.try(:full_name) || 'A pending member'} was successfully#{" and quietly" if @quite_reject} rejected." }
       end
     end
   end
