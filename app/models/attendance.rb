4dcc8ce00f316dd152aec6f6a1120ffc4a0a5dd8
Edward Guo <edwardplguo@gmail.com>
2015-09-10 20:33:53 -0400

    Fix attendance start_at and end_at input bug

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -3,37 +3,13 @@ class Attendance < ActiveRecord::Base
   enum reply: [:no_reply, :confirmed, :maybe, :declined]
 
   validate :end_at_after_start_at
-  validate :attended_requirements
-
-  attr_accessor :skip_end_at
-
-  after_initialize :set_skip_end_at
-  before_validation :set_skipped_end_at_nil
 
   belongs_to :member
   belongs_to :event, touch: true
 
   private
-  def set_skip_end_at
-    # Set to not skip if it is a new record
-    # Set to true on nil end_at otherwise
-    if self.skip_end_at.nil?
-      self.skip_end_at = new_record? ? false : self.end_at.nil?
-    end
-  end
-
-  def set_skipped_end_at_nil
-    self.end_at = nil if (self.skip_end_at == true || self.skip_end_at == "1")
-  end
-
   def end_at_after_start_at
     if start_at.present? && end_at.present? && (start_at > end_at)
-      errors.add(:end_at, "must after start at")
-    end
-  end
-
-  def attended_requirements
-    if start_at.present? && end_at.present?
       errors.add(:end_at, "must after start at")
     end
   end
