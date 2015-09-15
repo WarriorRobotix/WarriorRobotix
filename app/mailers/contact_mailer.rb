62d41e7d82f8d030da0826b7a44a807e9ecaa3ab
Edward Guo <edwardplguo@gmail.com>
2015-09-15 16:37:54 -0400

    Forward contact us messages to a different email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ApplicationMailer
   default :from => "contact-us@4659warriors.com"
   def contact_us_email(message)
     @message = message
-    mail(to: 'team4659.website@gmail.com', subject: 'A new contact us message')
+    mail(to: 'team4659@gmail.com', subject: "Contact-us message from: #{@message[:full_name]}")
   end
 end
