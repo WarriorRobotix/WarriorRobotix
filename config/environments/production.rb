7ade41c38cc1b2ed5e472e614c5e5baf71cd9eef
Edward Guo <edwardplguo@gmail.com>
2015-09-15 16:27:22 -0400

    Remove repeated time zone config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,6 @@ Rails.application.configure do
 
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-  config.time_zone = 'Eastern Time (US & Canada)'
 
   # Code is not reloaded between requests.
   config.cache_classes = true
