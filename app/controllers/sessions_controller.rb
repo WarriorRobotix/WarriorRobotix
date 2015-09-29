a0da9733bed2db7538f27bf5efb4ba9a15d4cdc4
Peiliang Guo <edwardplguo@gmail.com>
2015-09-29 15:27:09 -0400

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,18 +8,21 @@ class SessionsController < ApplicationController
     identifier = params[:identifier]
     password = params[:password]
     respond_to do |format|
+      flag = true
       if identifier.present? && password.present?
         if member = Member.where("(student_number = ? AND graduated_year IS NULL) OR email = ?", identifier, identifier).take.try(:authenticate, password)
           signin_member(member)
           cookies.permanent[:mtk] = "#{member.id}$#{member.remember_token}" if params[:remember_me] == '1'
           format.html { redirect_to root_path, notice: "You have successfully signed in" }
           format.json { render json: ['success', member.max_restriction] }
-          return
+          flag = false
         end
       end
-      flash.now[:alert] =  "Wrong email and password combination"
-      format.html { render :new }
-      format.json { render json: ['invalid', 0] }
+      if flag
+        flash.now[:alert] =  "Wrong email and password combination"
+        format.html { render :new }
+        format.json { render json: ['invalid', 0] }
+      end
     end
   end
 
