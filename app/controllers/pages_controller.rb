d21cb0d669efa66b88ce3a33f97b3a8ebc2014d8
Peiliang Guo <edwardplguo@gmail.com>
2015-12-28 15:20:35 -0500

    Cache latest posts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,7 @@ class PagesController < ApplicationController
   def home
     set_meta_tags_for_home
     @show_side_buttons = true
+    @latest_posts = Post.where(type: nil, restriction: 0).order(created_at: :DESC).limit(3).select(:id, :title, :description)
   end
 
   def vex
