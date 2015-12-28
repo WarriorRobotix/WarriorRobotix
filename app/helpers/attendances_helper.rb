e11a3b6b92483343c5a06202d9ec94c347bc1dad
Peiliang Guo <edwardplguo@gmail.com>
2015-12-28 15:36:42 -0500

    Clean start

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -1,13 +1,2 @@
 module AttendancesHelper
-  def load_attendances_for_center
-    members = Member.order(first_name: :ASC, last_name: :ASC).all.select(:id, :first_name, :last_name, :grade, :team_id)
-
-    today = Time.zone.now
-    today_attendances = Attendance.where(start_at: today.beginning_of_day..today.end_of_day).all
-
-    today_attendance_ids = Set.new( today_attendances.map { |a| a.id } )
-
-    @unchecked_members = []
-    @unchecked_member_teams = Hash.new
-  end
 end
