85aee5ec926148a58e362f3de7038ae227f39081
Peiliang Guo <edwardplguo@gmail.com>
2015-12-29 12:25:32 -0500

    Allow HTML in markdown

--- app/objects/markdown_render.rb
+++ app/objects/markdown_render.rb
@@ -1,7 +1,7 @@
 class MarkdownRender
   class << self
     def render(text)
-      @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(escape_html: true, hard_wrap: true ), autolink: true, strikethrough: true, tables: true, underline: true)
+      @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(escape_html: false, hard_wrap: true ), autolink: true, strikethrough: true, tables: true, underline: true)
       @markdown.render(text)
     end
   end
