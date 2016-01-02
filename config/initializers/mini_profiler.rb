5fba696c2c4051f3ee97abdb3fb2d9768f5c8068
Peiliang Guo <edwardplguo@gmail.com>
2016-01-02 11:50:13 -0500

    Try different mini_profiler initializer

--- config/initializers/mini_profiler.rb
+++ config/initializers/mini_profiler.rb
@@ -1,5 +1,14 @@
 # config/initializers/mini_profiler.rb
 
-if Rails.env.production?
-  Rack::MiniProfiler.config.storage = Rack::MiniProfiler::MemcacheStore
+# If Mini Profiler is included via gem
+if Rails.configuration.respond_to?(:load_mini_profiler) && Rails.configuration.load_mini_profiler
+  require 'rack-mini-profiler'
+  # initialization is skipped so trigger it
+  Rack::MiniProfilerRails.initialize!(Rails.application)
+end
+
+if defined?(Rack::MiniProfiler)
+  if Rails.env.production?
+    Rack::MiniProfiler.config.storage = Rack::MiniProfiler::MemcacheStore
+  end
 end
