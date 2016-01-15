43d7aa024d352ed44486072a5ac957a9ca774d42
Peiliang Guo <edwardplguo@gmail.com>
2016-01-14 20:48:04 -0500

    Remove raise_in_transactional_callback

--- config/application.rb
+++ config/application.rb
@@ -26,9 +26,6 @@ module WarriorRobotix
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-
     config.action_dispatch.rescue_responses.merge!('Forbidden' => :forbidden)
 
     config.action_view.field_error_proc = Proc.new { |html_tag, instance|
