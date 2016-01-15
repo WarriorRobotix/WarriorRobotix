71d98b4b99ebe9d42ab64575e98800990f49937d
Peiliang Guo <edwardplguo@gmail.com>
2016-01-14 21:11:35 -0500

    Auto truncate youtube link

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,12 @@ class Video < ApplicationRecord
   validates :title, presence: true
   validates :youtube_vid, presence: true
   validates :upload_date, presence: true
+
+  def youtube_vid=(value)
+    if /(?:v=|be\/)(?<vid>[A-Za-z0-9_\-]+)/ =~ value.to_s
+      super(Regexp.last_match(:vid))
+    else
+      super(value)
+    end
+  end
 end
--- test/models/video_test.rb
+++ test/models/video_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class VideoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "video should accept youtube ID" do
+     video = Video.new(youtube_vid: "ZtGAtP-YmeQ")
+     assert_equal("ZtGAtP-YmeQ", video.youtube_vid)
+  end
+
+  test "video should accept youtube.com link" do
+     video = Video.new(youtube_vid: "https://www.youtube.com/watch?v=ZtGAtP-YmeQ")
+     assert_equal("ZtGAtP-YmeQ", video.youtube_vid)
+  end
+
+  test "video should accept youtu.be link" do
+     video = Video.new(youtube_vid: "https://youtu.be/ZtGAtP-YmeQ")
+     assert_equal("ZtGAtP-YmeQ", video.youtube_vid)
+  end
+
+  test "video should accept youtube.com link without https" do
+     video = Video.new(youtube_vid: "www.youtube.com/watch?v=ZtGAtP-YmeQ")
+     assert_equal("ZtGAtP-YmeQ", video.youtube_vid)
+  end
 end
