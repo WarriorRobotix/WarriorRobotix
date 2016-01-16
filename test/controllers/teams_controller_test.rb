cde8b4af075a4c462eb2673942b994abb487f35d
Peiliang Guo <edwardplguo@gmail.com>
2016-01-16 07:31:57 -0500

    Add TeamsControllerTest

--- test/controllers/teams_controller_test.rb
+++ test/controllers/teams_controller_test.rb
@@ -1,51 +1,56 @@
 require 'test_helper'
 
-class TeamsControllerTest < ActionController::TestCase
-=begin
+class TeamsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @team = teams(:one)
+    sign_in_as_admin
   end
 
   test "should get index" do
-    get :index
+    get teams_url
     assert_response :success
-    assert_not_nil assigns(:teams)
+  end
+
+  test "should get team-editor" do
+    get team_editor_url
+    assert_response :success
   end
 
   test "should get new" do
-    get :new
+    get new_team_url
     assert_response :success
   end
 
   test "should create team" do
     assert_difference('Team.count') do
-      post :create, team: {  }
+      post teams_url, params: { team: { name: @team.name, image_link: @team.image_link } }
     end
 
-    assert_redirected_to team_path(assigns(:team))
+    assert_redirected_to team_editor_url
   end
 
-  test "should show team" do
-    get :show, id: @team
-    assert_response :success
+  test "shouldn't show single team" do
+    #The only way to view team should be teams#index
+    assert_raises(ActionController::RoutingError) do
+      get team_url(@team)
+    end
   end
 
   test "should get edit" do
-    get :edit, id: @team
+    get edit_team_url(@team)
     assert_response :success
   end
 
   test "should update team" do
-    patch :update, id: @team, team: {  }
-    assert_redirected_to team_path(assigns(:team))
+    patch team_url(@team), params: { team: { name: @team.name, image_link: @team.image_link } }
+    assert_redirected_to team_editor_url
   end
 
   test "should destroy team" do
     assert_difference('Team.count', -1) do
-      delete :destroy, id: @team
+      delete team_url(@team)
     end
 
-    assert_redirected_to teams_path
+    assert_redirected_to team_editor_url
   end
-=end
 end
