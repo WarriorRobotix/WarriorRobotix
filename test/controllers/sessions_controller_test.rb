32b2fbf67e767861b9741b403e3baf7629bcc055
Peiliang Guo <edwardplguo@gmail.com>
2016-01-15 19:03:07 -0500

    Add session controller tests

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
-class SessionsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class SessionsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @admin = members(:admin)
+    @member = members(:member)
+    @faker = Member.new(student_number: "123", email: "faker@example.com")
+  end
+
+  test "should sign in admin" do
+    post signin_url, params: { identifier: @admin.student_number, password: '123456' }
+    assert_redirected_to root_path
+  end
+
+  test "should sign in admin with email" do
+    post signin_url, params: { identifier: @admin.email, password: '123456' }
+    assert_redirected_to root_path
+  end
+
+  test "should sign in member" do
+    post signin_url, params: { identifier: @member.student_number, password: '123456' }
+    assert_redirected_to root_path
+  end
+
+  test "shouldn't sign in faker" do
+    post signin_url, params: { identifier: @faker.student_number, password: '123456' }
+    assert_response :success
+  end
+
+  test "shouldn't sign in incorrect password" do
+    post signin_url, params: { identifier: @admin.student_number, password: '654321' }
+    assert_response :success
+  end
 end
