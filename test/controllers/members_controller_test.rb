1a3a2615654128700762992fc89f6b1e085be8cf
Peiliang Guo <edwardplguo@gmail.com>
2016-01-16 07:45:18 -0500

    Add MembersControllerTest

--- test/controllers/members_controller_test.rb
+++ test/controllers/members_controller_test.rb
@@ -1,51 +1,49 @@
 require 'test_helper'
 
-class MembersControllerTest < ActionController::TestCase
-=begin
+class MembersControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @member = members(:edward)
+    @member = members(:member)
+    sign_in_as_admin
   end
 
   test "should get index" do
-    get :index
+    get members_url
     assert_response :success
-    assert_not_nil assigns(:members)
   end
 
   test "should get new" do
-    get :new
+    get new_member_url
     assert_response :success
   end
 
   test "should create member" do
     assert_difference('Member.count') do
-      post :create, member: {  }
-    end
-
-    assert_redirected_to member_path(assigns(:member))
-  end
-
-  test "should show member" do
-    get :show, id: @member
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @member
-    assert_response :success
-  end
-
-  test "should update member" do
-    patch :update, id: @member, member: {  }
-    assert_redirected_to member_path(assigns(:member))
-  end
-
-  test "should destroy member" do
-    assert_difference('Member.count', -1) do
-      delete :destroy, id: @member
+      post members_url, params: { member: { accepted: @member.accepted, admin: @member.admin, email: "second.member@example.com", first_name: "Second", grade: @member.grade, graduated_year: @member.graduated_year, last_name: "Example", student_number: "333", title: @member.title } }
     end
 
     assert_redirected_to members_path
   end
-=end
+
+  test "should show member" do
+    get member_url(@member)
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get edit_member_url(@member)
+    assert_response :success
+  end
+
+  test "should update member" do
+    patch member_url(@member), params: { member: { accepted: @member.accepted, admin: @member.admin, email: @member.email, first_name: @member.first_name, grade: @member.grade, graduated_year: @member.graduated_year, last_name: @member.last_name, student_number: @member.student_number, title: @member.title } }
+    assert_redirected_to members_path
+  end
+
+  test "should destroy member" do
+    assert_difference('Member.count', -1) do
+      delete member_url(@member)
+    end
+
+    assert_redirected_to members_path
+  end
 end
