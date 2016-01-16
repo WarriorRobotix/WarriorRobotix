c06d63e8536bccd5094d703e5fe5b65a4f353474
Peiliang Guo <edwardplguo@gmail.com>
2016-01-16 07:56:15 -0500

    Add SponsorsControllerTest

--- test/controllers/sponsors_controller_test.rb
+++ test/controllers/sponsors_controller_test.rb
@@ -1,51 +1,50 @@
 require 'test_helper'
 
-class SponsorsControllerTest < ActionController::TestCase
-=begin
+class SponsorsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @sponsor = sponsors(:one)
+    sign_in_as_admin
   end
 
   test "should get index" do
-    get :index
+    get sponsors_url
     assert_response :success
-    assert_not_nil assigns(:sponsors)
   end
 
   test "should get new" do
-    get :new
+    get new_sponsor_url
     assert_response :success
   end
 
   test "should create sponsor" do
     assert_difference('Sponsor.count') do
-      post :create, sponsor: { description: @sponsor.description, facebook_link: @sponsor.facebook_link, image_link: @sponsor.image_link, name: @sponsor.name, twitter_link: @sponsor.twitter_link, website_link: @sponsor.website_link }
-    end
-
-    assert_redirected_to sponsor_path(assigns(:sponsor))
-  end
-
-  test "should show sponsor" do
-    get :show, id: @sponsor
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @sponsor
-    assert_response :success
-  end
-
-  test "should update sponsor" do
-    patch :update, id: @sponsor, sponsor: { description: @sponsor.description, facebook_link: @sponsor.facebook_link, image_link: @sponsor.image_link, name: @sponsor.name, twitter_link: @sponsor.twitter_link, website_link: @sponsor.website_link }
-    assert_redirected_to sponsor_path(assigns(:sponsor))
-  end
-
-  test "should destroy sponsor" do
-    assert_difference('Sponsor.count', -1) do
-      delete :destroy, id: @sponsor
+      post sponsors_url, params: { sponsor: { description: @sponsor.description, facebook_link: @sponsor.facebook_link, image_link: @sponsor.image_link, name: @sponsor.name, twitter_link: @sponsor.twitter_link, website_link: @sponsor.website_link } }
     end
 
     assert_redirected_to sponsors_path
   end
-=end
+
+  test "should show sponsor" do
+    assert_raises(ActionController::RoutingError) do
+      get sponsor_url(@sponsor)
+    end
+  end
+
+  test "should get edit" do
+    get edit_sponsor_url(@sponsor)
+    assert_response :success
+  end
+
+  test "should update sponsor" do
+    patch sponsor_url(@sponsor), params: { sponsor: { description: @sponsor.description, facebook_link: @sponsor.facebook_link, image_link: @sponsor.image_link, name: @sponsor.name, twitter_link: @sponsor.twitter_link, website_link: @sponsor.website_link } }
+    assert_redirected_to sponsors_path
+  end
+
+  test "should destroy sponsor" do
+    assert_difference('Sponsor.count', -1) do
+      delete sponsor_url(@sponsor)
+    end
+
+    assert_redirected_to sponsors_path
+  end
 end
