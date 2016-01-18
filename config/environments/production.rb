af6654797e1c6a914762d7fb714d7e518fcb28e8
Peiliang Guo <edwardplguo@gmail.com>
2016-01-18 17:06:30 -0500

    CDN assets through fastly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   config.cache_store = :dalli_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = 'http://4659warriors.com.global.prod.fastly.net'
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
