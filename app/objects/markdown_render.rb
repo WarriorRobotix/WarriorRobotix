b01f0864d5c40f2299dc86f240519cb88773aced
Peiliang Guo <edwardplguo@gmail.com>
2016-01-21 17:59:53 -0500

    Add MarkdownRender.stripdown(text)

--- app/objects/markdown_render.rb
+++ app/objects/markdown_render.rb
@@ -1,8 +1,14 @@
 class MarkdownRender
+  require 'redcarpet/render_strip'
   class << self
     def render(text)
       @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(escape_html: true, hard_wrap: true ), autolink: true, strikethrough: true, tables: true, underline: true)
       @markdown.render(text)
+    end
+
+    def stripdown(text)
+      @stripdown_render ||= Redcarpet::Markdown.new(Redcarpet::Render::StripDown)
+      @stripdown_render.render(text)
     end
   end
 end
