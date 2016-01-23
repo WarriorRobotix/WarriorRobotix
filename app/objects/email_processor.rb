10f608e4f3639fd0b33ab954ea04e266209625c9
Peiliang Guo <edwardplguo@gmail.com>
2016-01-23 10:10:17 -0500

    Fix attachment bug

--- app/objects/email_processor.rb
+++ app/objects/email_processor.rb
@@ -5,6 +5,9 @@ class EmailProcessor
 
   def process
     email_hash = {from: @email.from[:full], to: @email.to, subject: @email.subject, body: @email.body, timestamp: Time.zone.now.to_s}
-    ContactMailer.forwarding_email(email_hash, @email.attachments).deliver_later
+    attachments = @email.attachments.map do |att|
+      {filename: att.original_filename, data: File.read(att.tempfile)}
+    end
+    ContactMailer.forwarding_email(email_hash, attachments).deliver_later
   end
 end
