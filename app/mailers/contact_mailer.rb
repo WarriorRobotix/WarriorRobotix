4ffae416099fa44d6224ab2df9e47dd984ecf2a7
Peiliang Guo <edwardplguo@gmail.com>
2016-01-23 09:58:02 -0500

    Friendlier "from" address in ContactMailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ApplicationMailer
-  default :from => "Contact Us 4659WARRIORS.COM <contact-us@4659warriors.com>"
+  default :from => "Contact Us - Website <contact-us@4659warriors.com>"
   def contact_us_email(message)
     @message = message
     mail(to: ENV['FORWARD_EMAIL_TO_ADDRESS'], subject: "Contact-us message from: #{@message[:full_name]}")
@@ -10,6 +10,6 @@ class ContactMailer < ApplicationMailer
     original_attachments.each do |original_att|
       attachments[original_att.original_filename] = File.read(original_att.tempfile)
     end
-    mail(to: ENV['FORWARD_EMAIL_TO_ADDRESS'], from: "Forward 4659WARRIORS.COM <forward@4659warriors.com>", subject: "#{@email[:to][0][:email]} forward: #{@email[:subject]}")
+    mail(to: ENV['FORWARD_EMAIL_TO_ADDRESS'], from: "Forward - Website <forward@4659warriors.com>", subject: "#{@email[:to][0][:email]} forward: #{@email[:subject]}")
   end
 end
