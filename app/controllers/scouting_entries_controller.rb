f83efac1ae30e1cfd334831497af07cb3f962d3d
Peiliang Guo <edwardplguo@gmail.com>
2016-04-17 14:56:39 -0400

    Add rating to scouting entries

--- app/controllers/scouting_entries_controller.rb
+++ app/controllers/scouting_entries_controller.rb
@@ -3,7 +3,7 @@ class ScoutingEntriesController < ApplicationController
 
   skip_before_action :verify_authenticity_token
   skip_before_action :authenticate_admin!
-  
+
   # GET /scouting_entries
   # GET /scouting_entries.json
   def index
@@ -72,7 +72,7 @@ class ScoutingEntriesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def scouting_entry_params
-      params.require(:scouting_entry).permit(:team_stat_id, :member_id, :extra_note, :drive_motors, :drive_motor_type, :drive_wheels, :drive_wheel_type, :drive_configuration, :drive_clearance, :shooter_type, :shooter_motors, :shooter_rpm, :intake_type, :intake_motors, :intake_motor_type, :intake_flip_capacity,
+      params.require(:scouting_entry).permit(:team_stat_id, :member_id, :extra_note, :rating, :drive_motors, :drive_motor_type, :drive_wheels, :drive_wheel_type, :drive_configuration, :drive_clearance, :shooter_type, :shooter_motors, :shooter_rpm, :intake_type, :intake_motors, :intake_motor_type, :intake_flip_capacity,
       :lift, :lift_motors, :lift_elevation, :lift_works, :driver_consistency, :driver_intelligence, :preloads_capacity, :shooter_consistency, :shooter_range, :autonomous_strategy, :autonomous_preload_points, :autonomous_field_points, :autonomous_reliability, :stalling, :connection_issues)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160416013958) do
+ActiveRecord::Schema.define(version: 20160417185406) do
 
   create_table "attendances", force: :cascade do |t|
     t.integer  "member_id",              null: false
@@ -192,6 +192,7 @@ ActiveRecord::Schema.define(version: 20160416013958) do
     t.string   "connection_issues"
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+    t.integer  "rating"
   end
 
   add_index "scouting_entries", ["member_id"], name: "index_scouting_entries_on_member_id"
