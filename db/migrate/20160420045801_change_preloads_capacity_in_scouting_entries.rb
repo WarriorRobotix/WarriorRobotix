93d4270c098aa2213f04611db04f720e4f07c926
Peiliang Guo <edwardplguo@gmail.com>
2016-04-20 01:10:30 -0400

    Fix migration

--- db/migrate/20160420045801_change_preloads_capacity_in_scouting_entries.rb
+++ db/migrate/20160420045801_change_preloads_capacity_in_scouting_entries.rb
@@ -1,9 +1,9 @@
 class ChangePreloadsCapacityInScoutingEntries < ActiveRecord::Migration
   def up
-    change_column :scouting_entries, :preloads_capacity, :integer
+    change_column :scouting_entries, :preloads_capacity, 'integer USING CAST(preloads_capacity AS integer)'
   end
 
   def down
-    change_column :scouting_entries, :preloads_capacity, :string
+    change_column :scouting_entries, :preloads_capacity, 'string USING CAST(preloads_capacity AS string)'
   end
 end
